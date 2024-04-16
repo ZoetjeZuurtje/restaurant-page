193789eaabcfa11cf4f86ac04963824a190c2cf4
Bram <bramstrik@live.com>
2024-04-16 14:22:05 +0200

    Webpack setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,33 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
  module.exports = {
    entry: {
-     index: './src/index.js',
+     script: './src/index.js',
    },
 
   plugins: [
 
     new HtmlWebpackPlugin({
-
-      title: 'Output Management',
-
+      title: 'Restaurant Page',
+      template: 'src/index.html',
     }),
+    new MiniCssExtractPlugin(),
 
   ],
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
      clean: true,
+   },
+   module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+        }
+    ]
    },
    mode: "development",
 };
